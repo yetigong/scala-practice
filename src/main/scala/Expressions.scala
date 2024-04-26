759f3601860115b4e057eae58283d1ce04abc443
yetigong <panbotoy@gmail.com>
2024-04-25 23:56:17 -0700

    test commit

---
+++ src/main/scala/Expressions.scala
@@ -0,0 +1,14 @@
+object Expressions {
+  val mathExpressions = 2 + 3 * 5
+
+  val codeBlock = {
+    val localVal = 78
+    localVal + 10
+  }
+
+  val somethingElse = 1
+  def main(args: Array[String]): Unit = {
+    print(codeBlock)
+    print("abc")
+  }
+}
